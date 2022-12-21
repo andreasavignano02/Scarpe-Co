e0a73fe75f036698cbd97f7e636dd775725d79ec
andrea savignano <112469730+andreasavignano02@users.noreply.github.com>
2022-12-21 15:19:50 +0100

    start function with server

--- Scarpe-Co/Scarpe-Co/Controllers/HomeController.cs
+++ Scarpe-Co/Scarpe-Co/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Scarpe_Co.Models;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
@@ -12,9 +14,35 @@ namespace Scarpe_Co.Controllers
         public ActionResult Index()
         {
             SqlConnection con = new SqlConnection();
+            con.ConnectionString = ConfigurationManager.ConnectionStrings["ScarpeDab"].ToString();
             con.Open();
+
+            SqlCommand command = new SqlCommand();
+            command.CommandText = "Select * from ArticoliTab";
+            command.Connection= con;
+
+            SqlDataReader reader = command.ExecuteReader();
+            List<Articoli> articololist = new List<Articoli>();
+
+            if(reader.HasRows)
+            {
+                while(reader.Read())
+                {
+                    Articoli articolo = new Articoli();
+                    articolo.IDArticolo = Convert.ToInt32(reader["IDArticolo"]);
+                    articolo.NomeArticolo = reader["NomeArticolo"].ToString();
+                    articolo.Costo = Convert.ToDouble(reader["Costo"]);
+                    articolo.DescrizioneArticolo = reader["DescrizioneArticolo"].ToString();
+                    articolo.IMGUrl = reader["IMGUrl"].ToString();
+                    articolo.IMGGallery1 = reader["IMGGallery1"].ToString();
+                    articolo.IMGGallery2 = reader["IMGGallery2"].ToString();
+                    articolo.Visibile = Convert.ToBoolean(reader["Visibile"]);
+                    articololist.Add(articolo);
+                }
+            }
+
             con.Close();
-            return View();
+            return View(articololist);
         }
     }
 }
--- Scarpe-Co/Scarpe-Co/Models/Articoli.cs
+++ Scarpe-Co/Scarpe-Co/Models/Articoli.cs
@@ -1,9 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Xml.Linq;
 
 namespace Scarpe_Co.Models
 {
@@ -16,24 +16,24 @@ namespace Scarpe_Co.Models
         public string NomeArticolo { get; set; }
 
         [Display(Name = "Costo dell'articolo")]
-        [Required(ErrorMessage ="Inserire un prezzo")]
+        [Required(ErrorMessage = "Inserire un prezzo")]
         public double Costo { get; set; }
 
         [Display(Name = "Descrizione dell'articolo")]
         [Required(ErrorMessage = "Inserire una descrizione")]
-        public string DescrizioneArticolo  { get; set; }
+        public string DescrizioneArticolo { get; set; }
 
         [Display(Name = "Immagine di copertina dell'articolo")]
         [Required(ErrorMessage = "Inserire un' immagine di copertina")]
-        public string IMGUrl  { get; set; }
+        public string IMGUrl { get; set; }
 
         [Display(Name = "Prima immagine dell'articolo")]
         [Required(ErrorMessage = "Inserire un'immagine")]
-        public double IMGGallery1 { get; set; }
+        public string IMGGallery1 { get; set; }
 
         [Display(Name = "Prima immagine dell'articolo")]
         [Required(ErrorMessage = "Inserire un'immagine")]
-        public double IMGGallery2 { get; set; }
+        public string IMGGallery2 { get; set; }
 
         public bool Visibile { get; set; }
     }
