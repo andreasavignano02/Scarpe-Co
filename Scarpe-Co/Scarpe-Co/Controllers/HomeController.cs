48a4a0d0be12e66e3f9fe391f2ad4c5fcb50776e
andrea savignano <112469730+andreasavignano02@users.noreply.github.com>
2022-12-22 09:40:05 +0100

    finish exercise

--- Scarpe-Co/Scarpe-Co/Controllers/HomeController.cs
+++ Scarpe-Co/Scarpe-Co/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data.SqlClient;
+using System.Drawing;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -18,7 +19,7 @@ namespace Scarpe_Co.Controllers
             con.Open();
 
             SqlCommand command = new SqlCommand();
-            command.CommandText = "Select * from ArticoliTab";
+            command.CommandText = "Select * from ArticoliTab where visibile = 1";
             command.Connection= con;
 
             SqlDataReader reader = command.ExecuteReader();
@@ -85,6 +86,64 @@ namespace Scarpe_Co.Controllers
             }
             con.Close();
             return View(articolo);
+        }
+
+        [HttpGet]
+        public ActionResult ChangeVisible(int id)
+        {
+            SqlConnection con = new SqlConnection();
+            Articoli articolo = new Articoli();
+            try
+            {
+                con.ConnectionString = ConfigurationManager.ConnectionStrings["ScarpeDab"].ToString();
+                con.Open();
+
+                SqlCommand cmd = new SqlCommand();
+                cmd.Parameters.AddWithValue("@ID", id);
+                cmd.CommandText = "select * from ArticoliTab where IDArticolo = @ID";
+                cmd.Connection = con;
+                SqlDataReader reader = cmd.ExecuteReader();
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        articolo.IDArticolo = id;
+                        articolo.Visibile = Convert.ToBoolean(reader["Visibile"]);
+                    }
+                }
+            }
+            catch(Exception ex) 
+            { 
+                con.Close(); 
+            }
+            con.Close();
+            return View(articolo);
+        }
+
+        [HttpPost]
+        public ActionResult ChangeVisible(Articoli articolo, int id) 
+        {
+            SqlConnection con = new SqlConnection();
+            try
+            {
+                con.ConnectionString = ConfigurationManager.ConnectionStrings["ScarpeDab"].ToString();
+                con.Open();
+
+                SqlCommand command = new SqlCommand();
+                command.Parameters.AddWithValue("@ID", id);
+                command.Parameters.AddWithValue("Visibile", articolo.Visibile);
+
+                command.CommandText = "UPDATE ArticoliTab SET Visibile = @Visibile WHERE IDArticolo = @ID";
+                command.Connection = con;
+
+                command.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                con.Close();
+            }
+            con.Close();
+            return RedirectToAction("index");
         }
     }
 }
